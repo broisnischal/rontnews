cb793d346a5d5d10c1829735066c786a182eadca
broisnischal <neeswebservice@gmail.com>
2024-03-06 00:23:17 +0545

    feat:switched-to-vite

--- app/routes/edge.tsx
+++ app/routes/edge.tsx
@@ -21,14 +21,12 @@ export default function Edge() {
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Can you defer on the edge? Deployment</h1>
+      <h1>Can you defer on the edge?</h1>
       <Suspense fallback="Loading...">
         <Await resolve={message}>{(message) => <p>{message}</p>}</Await>
       </Suspense>
       <br />
-      <Link to="/" className="text-4xl">
-        Home
-      </Link>
+      <Link to="/">Home</Link>
     </div>
   );
 }
